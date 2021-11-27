94e309a958b4ba8ad6c766d2e2f4d13e59a869d6
VIctor <colen.victor@icloud.com>
2021-11-27 02:47:03 -0300

    Initial Commit

---
+++ Rock, Paper, Win/Rock__Paper__WinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock__Paper__WinApp.swift
+//  Rock, Paper, Win
+//
+//  Created by Victor Colen on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Rock__Paper__WinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
